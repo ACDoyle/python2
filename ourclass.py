fee76144a8c6999f7e6a5536fcb184e548c59c69
mike <mike@o2.pl>
2018-08-25 10:50:54 -0400

    final work on ourclass

--- ourclass.py
+++ ourclass.py
@@ -2,6 +2,25 @@ class OurClass(object):
   def __init__(self, a):
     self.OurAtrr = a
 
+  @property
+  def OurAtrr(self):
+    return self.__OurAtrr
+
+  @OurAtrr.setter
+  def OurAtrr(self, val):
+    if val < 0:
+      self.__OurAtrr = 0
+    elif val > 10:
+      self.__OurAtrr = 10
+    else:
+      self.__OurAtrr = val
+
 if __name__ == "__main__":
+  x = OurClass(-100)
+  print(x.OurAtrr)
+  x = OurClass(3)
+  print(x.OurAtrr)
   x = OurClass(100)
+  print(x.OurAtrr)
+  x = OurClass(8.9)
   print(x.OurAtrr)
