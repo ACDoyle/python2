fe6f94d4bc267855d5ca0f59f819e999a4dd7313
mike <mike@flask-server.example.com>
2018-08-25 10:11:50 -0400

    correct initial project

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 from mutators import P
 p1 = P(43)
-print "p1 = {0}".format(p1.get_x())
+#print "p1 = {0}".format(p1.get_x())
+p2 = P(4711)
+print p1.get_x()
+p1.set_x(99)
+p1.set_x(p1.get_x() + p2.get_x())
+print p1.get_x()
