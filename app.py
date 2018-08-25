04de13f7f513511147687bf9714147e34b7152ab
mike <mike@flask-server.example.com>
2018-08-25 10:38:24 -0400

    working setters and getters

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
 from mutators import P
-p1 = P(3333)
+p1 = P(200)
 #print "p1 = {0}".format(p1.get_x())
-p2 = P(4711)
-print p1.get_x()
-p1.set_x(99)
-p1.set_x(p1.get_x() + p2.get_x())
-print p1.get_x()
+p2 = P(-5)
+print p1.x
+print p2.x
+print "#####################"
+p1.x=-99
+
+p1.x=(p1.x + p2.x)
+print p1.x
--- mutators.py
+++ mutators.py
@@ -1,14 +1,17 @@
-class P:
-  def __init__(self,x):
-    self.set_x(x)
- 
-  def get_x(self):
-    return self.__x
+class P(object):
 
-  def set_x(self, x):
-    if x < 0:
-      self.__x = 0
-    elif x > 100:
-    	self.__x = 100
-    else:
-    	self.__x = x
+    def __init__(self,x):
+        self.x = x
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if x < 0:
+            self.__x = 0
+        elif x > 100:
+            self.__x = 100
+        else:
+            self.__x = x
